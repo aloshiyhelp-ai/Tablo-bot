5725488a05312a32c37b4fc7ff6818dd0db6878e
aloshiyhelp-ai <aloshiyhelp@gmail.com>
2025-12-20 00:20:59 +0600

    Load configuration from .env file
    Refactor configuration to use environment variables.

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-TOKEN = "8568086831:AAF5idiBW0T0V6EuQXZk_XFMwlO64fpULrg"
-OWNER_ID = 889344406  # o‘zingning Telegram ID
-BOT_NAME = "TabloUzBot"  # Bu yerga botingizning ismini yozasiz
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # .env faylni yuklaydi
+
+TOKEN = os.getenv("TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+BOT_NAME = os.getenv("BOT_NAME")
