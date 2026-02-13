2ec2bc5d6a6b7264e15d7826b8db8a0150514f32
aloshiyhelp-ai <aloshiyhelp@gmail.com>
2026-02-13 06:11:34 +0600

    Refactor environment variable usage in config.py
    Updated environment variable retrieval for TOKEN and BOT_NAME.

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()  # .env faylni yuklaydi
 
-TOKEN = os.getenv("8415627925:AAGtZRXuSzHMT78CoGtk4AAQ65Bbni7o5kM")
+TOKEN = os.getenv("TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-BOT_NAME = os.getenv("AlOshiy")
+BOT_NAME = os.getenv("BOT_NAME")
